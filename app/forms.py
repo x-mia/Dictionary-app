b37613202d84930344f9d90563ac4599f9f794f4
Michaela Denisova <michaeladenisova@gmail.com>
2019-12-14 23:34:39 +0200

    frontend upgraded

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 class WordForm(FlaskForm):
-    word = StringField('Sisesta sõna', validators=[DataRequired()])
+    word = StringField('Sisesta sõna', validators=[DataRequired(), Length(1, 64)])
+    language = SelectField('Language', choices=[('est-svk', 'Eesti-Slovaki'), ('svk-est', 'Slovaki-Eesti')])
     submit = SubmitField('Tõlgi')
+class SlovakWordForm(FlaskForm):
+    word = StringField('Zadaj slovo', validators=[DataRequired(), Length(1, 64)])
+    language = SelectField('Jazyk', choices=[('est-svk', 'Estónsko-Slovenský'), ('svk-est', 'Slovensko-Estónsky')])
+    submit = SubmitField('Prelož')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, url_for, redirect, flash
-from app.forms import WordForm
+from app.forms import WordForm, SlovakWordForm
 from app.word_finding import get_meaning
 from pandas import read_csv
 
@@ -13,10 +13,33 @@ def index():
         return redirect(url_for('dictionary', word=form.word.data))
     return render_template('index.html', form=form)
 
+@app.route('/base_svk', methods=['GET', 'POST'])
+def base_svk():
+    form = SlovakWordForm()
+    if form.validate_on_submit():
+        return redirect(url_for('dictionary_svk', word=form.word.data))
+    return render_template('base-svk.html', form=form)
+
 @app.route('/dictionary/<word>')
 def dictionary(word):
+    form = WordForm()
+    if form.validate_on_submit():
+        return redirect(url_for('dictionary', form=form, word=form.word.data))
+
     data = read_csv("sonaraamat4.csv")
     entry = get_meaning(word, data)
     if not entry:
-        return render_template('404.html', word=word, entry=entry)
-    return render_template('dictionary.html', word=word, entry=entry)
+        return render_template('404.html', form=form, word=word, entry=entry)
+    return render_template('dictionary.html', form=form, word=word, entry=entry)
+
+@app.route('/dictionary_svk/<word>')
+def dictionary_svk(word):
+    form = SlovakWordForm()
+    if form.validate_on_submit():
+        return redirect(url_for('dictionary_svk', form=form, word=form.word.data))
+
+    data = read_csv("sonaraamat4.csv")
+    entry = get_meaning(word, data)
+    if not entry:
+        return render_template('404-svk.html', form=form, word=word, entry=entry)
+    return render_template('dictionary-svk.html', form=form, word=word, entry=entry)
